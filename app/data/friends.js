2dd3b4a325c7c1e35b9468d0b0ac4a0b402774f2
Brian Christopher <bcwood79@yahoo.com>
2019-07-07 21:32:19 -0400

    Home page coded. Server routes appear to be working. Object array started with three examples.

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+// ===============================================================================
+// DATA
+// This array holds the data for all of our firends.
+// ===============================================================================
+
+var superFriendsArray = [
+    {
+        id: "aquaman",
+        name: "Aquaman",
+        photo: ".\app\images\Aquaman.png",
+        about: "I an aquatic superhero with the ability to breathe underwater. I posess super strength and the ability to communicate with marine life. At work I am the King of the undersea city of Atlantis. I'm also a founding member of the SuperFriends. I like to get around on a large seahorse named Storm. And I love the music of the Beatles.",
+        trait0: 10,
+        trait1: 10,
+        trait2: 10,
+        trait3: 10,
+        trait4: 10,
+        trait5: 10,
+        trait6: 10,
+        trait7: 10,
+        trait8: 10,
+        trait9: 10
+    },
+    {
+        id: "batman",
+        name: "Batman",
+        photo: ".\app\images\Batman.png",
+        about: "Unlike most superheroes, I do not possess any superpowers; rather, I rely on my genius intellect, physical prowess, martial arts abilities, detective skills, science and technology, vast wealth, intimidation, and indomitable will.  I like to drive fast cars, especially my Batmobile. And did I mention I'm filthy rich? ",
+        trait0: 8,
+        trait1: 8,
+        trait2: 8,
+        trait3: 8,
+        trait4: 8,
+        trait5: 8,
+        trait6: 8,
+        trait7: 8,
+        trait8: 8,
+        trait9: 8
+    },
+    {
+        id: "robin",
+        name: "Robin",
+        photo: ".\app\images\Robin.png",
+        about: `Hi, I'm Robin, the Boy Wonder. But you can call me Richard. My friends call me "Dick." I like wearing tights and riding in fast cars with older men. My celebrity crush is Casey Kasem and I love listening to the top 40.`,
+        trait0: 5,
+        trait1: 5,
+        trait2: 5,
+        trait3: 5,
+        trait4: 5,
+        trait5: 5,
+        trait6: 5,
+        trait7: 5,
+        trait8: 5,
+        trait9: 5
+    }
+];
+
+
+// This exports the array, making it accessible to other files using require.
+module.exports = superFriendsArray;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,42 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+// =============================================================
+
+
+
+module.exports = function(app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+  
+    app.get("/", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+  
+    app.get("/survey", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.get("/app/images/SuperfriendsStanding.png", function(req, res) {
+        res.sendFile(path.join(__dirname, "../images/SuperfriendsStanding.png"));
+      });
+      
+    // If no matching route is found default to home
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+  };
