8fce5bb5ac4766fbe2742b8b2030af23c64a92bf
Brian Christopher <bcwood79@yahoo.com>
2019-07-08 19:19:25 -0400

    Fixed survey page so that it would take in data. Unsuccesfully worked on routing.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,56 +10,69 @@ var superFriendsArray = require("../data/friends.js");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
-  app.get("/api/superFriends", function(req, res) {
+  app.get("/api/superFriends", function (req, res) {
     res.json(superFriendsArray);
   });
- 
+
+
+  app.get("api/results", function (req, res) {
+    console.log(req.body.name)
+    // console.log(res)
+  });
+
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
   // ---------------------------------------------------------------------------
+  app.post("api/results", function (req, res) {
+    console.log("The post results were called. Hot damn.")
+    console.log(req.body.name)
+    console.log(req)
+    console.log(res)
+  });
 
 
-    // Note the code here. Our "server" will respond to a user"s survey result
-    // Then compare those results against every user in the database.
-    // It will then calculate the difference between each of the numbers and the user"s numbers.
-    // It will then choose the user with the least differences as the "best friend match."
-    // In the case of multiple users with the same result it will choose the first match.
-    // After the test, it will push the user to the database.
 
-    // We will use this object to hold the "best match". We will constantly update it as we
-    // loop through all of the options
- 
+  // Note the code here. Our "server" will respond to a user"s survey result
+  // Then compare those results against every user in the database.
+  // It will then calculate the difference between each of the numbers and the user"s numbers.
+  // It will then choose the user with the least differences as the "best friend match."
+  // In the case of multiple users with the same result it will choose the first match.
+  // After the test, it will push the user to the database.
 
-    // Here we take the result of the user"s survey POST and parse it.
-    
-    // This variable will calculate the difference between the user"s scores and the scores of
-    // each user in the database
-    
-
-    // Here we loop through all the friend possibilities in the database.
-    
-      // We then loop through all the scores of each friend
-      
-        // We calculate the difference between the scores and sum them into the totalDifference
-   
-
-      // If the sum of differences is less then the differences of the current "best match"
-
-        // Reset the bestMatch to be the new friend.
-     
-    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-    // the database will always return that the user is the user's best friend).
+  // We will use this object to hold the "best match". We will constantly update it as we
+  // loop through all of the options
 
 
-    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
-  };
+  // Here we take the result of the user"s survey POST and parse it.
+
+  // This variable will calculate the difference between the user"s scores and the scores of
+  // each user in the database
+
+
+  // Here we loop through all the friend possibilities in the database.
+
+  // We then loop through all the scores of each friend
+
+  // We calculate the difference between the scores and sum them into the totalDifference
+
+
+  // If the sum of differences is less then the differences of the current "best match"
+
+  // Reset the bestMatch to be the new friend.
+
+  // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+  // the database will always return that the user is the user's best friend).
+
+
+  // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -17,26 +17,35 @@ app.use(express.json());
 
 
 
-module.exports = function(app) {
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
-  
-    app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-  
-    app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
+module.exports = function (app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
 
-    app.get("/app/images/SuperfriendsStanding.png", function(req, res) {
-        res.sendFile(path.join(__dirname, "../images/SuperfriendsStanding.png"));
-      });
-      
-    // If no matching route is found default to home
-    app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-  };
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  app.get("/result/:match", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/app/images/SuperfriendsStanding.png", function (req, res) {
+    res.sendFile(path.join(__dirname, "../images/SuperfriendsStanding.png"));
+  });
+
+  app.get("/app/images/:image", function (req, res) {
+    console.log(req.params.image)
+    res.sendFile(path.join(__dirname, "../images/", req.params.image));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
